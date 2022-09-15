8766a0ac3ea5e30cef658688723fe4db5b86665c
Alexandre Almeida <alee.wxa@gmail.com>
2022-09-15 08:29:09 -0300

    feat: homepage

--- Bin2Dec/PHP/src/Views/index.php
+++ Bin2Dec/PHP/src/Views/index.php
@@ -1,5 +1,11 @@
-<div class="w-1/2 bg-slate-400 mx-auto mt-12">
-        <h1 class="font-bold text-lg text-center">Bin2Dec</h1>
-        <h2 class="font-bold">App Ideias</h2>
-        <p>Bin2Dec permite o usuário digitar até 8 dígitos binários, 0 e 1, em sequência mostra o valor decimal equivalente. </p>
-</div>
+<main class="bg-conic-to-b from-sky-400 to-blue-800 h-screen flex">
+    <div id="left" class="border-4 border-red-400 w-full">
+        <h1 class="font-bold text-6xl text-center relative mt-12">Bin2Dec</h1>
+        <h2 class="font-bold text-center mt-6 text-4xl">App Ideias</h2>
+        <p class="text-lg text-center mt-6">Bin2Dec permite o usuário digitar até 8 dígitos binários, 0 e 1, em sequência mostra o valor decimal equivalente. </p>
+
+    </div>
+    <div id="right" class="border-4 border-green-700 w-full">
+
+    </div>
+</main>
