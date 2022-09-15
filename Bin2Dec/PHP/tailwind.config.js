720b127d3b56ba22f8f8045f47b9efcec5f7f017
Alexandre Almeida <alee.wxa@gmail.com>
2022-09-14 23:10:28 -0300

    feat: tailwind

--- Bin2Dec/PHP/tailwind.config.js
+++ Bin2Dec/PHP/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,php}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./src/**/*.{html,js,php}", "./public/*.{php}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
