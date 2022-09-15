596d64bf66c3c2025909b531eec6dfe828558fb6
Alexandre Almeida <alee.wxa@gmail.com>
2022-09-14 23:09:11 -0300

    feat: views

---
+++ Bin2Dec/PHP/src/Views/index.php
@@ -0,0 +1,5 @@
+<div class="w-1/2 bg-slate-400 mx-auto mt-12">
+        <h1 class="font-bold text-lg text-center">Bin2Dec</h1>
+        <h2 class="font-bold">App Ideias</h2>
+        <p>Bin2Dec permite o usuário digitar até 8 dígitos binários, 0 e 1, em sequência mostra o valor decimal equivalente. </p>
+</div>
