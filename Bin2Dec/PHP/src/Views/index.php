1809828f0c48a6877cad02f28cf011caa7d532d8
Alexandre Almeida <alee.wxa@gmail.com>
2022-09-15 08:30:06 -0300

    fix: removido bordas

--- Bin2Dec/PHP/src/Views/index.php
+++ Bin2Dec/PHP/src/Views/index.php
@@ -1,11 +1,11 @@
 <main class="bg-conic-to-b from-sky-400 to-blue-800 h-screen flex">
-    <div id="left" class="border-4 border-red-400 w-full">
+    <div id="left" class="w-full">
         <h1 class="font-bold text-6xl text-center relative mt-12">Bin2Dec</h1>
         <h2 class="font-bold text-center mt-6 text-4xl">App Ideias</h2>
         <p class="text-lg text-center mt-6">Bin2Dec permite o usuário digitar até 8 dígitos binários, 0 e 1, em sequência mostra o valor decimal equivalente. </p>
 
     </div>
-    <div id="right" class="border-4 border-green-700 w-full">
+    <div id="right" class="w-full">
 
     </div>
 </main>
