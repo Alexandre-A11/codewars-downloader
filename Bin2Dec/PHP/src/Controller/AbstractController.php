477fc96e4da23681f4ae63bb593caccb79b25a61
Alexandre Almeida <alee.wxa@gmail.com>
2022-09-15 17:36:08 -0300

    feat: recebe variável para enviar para página

--- Bin2Dec/PHP/src/Controller/AbstractController.php
+++ Bin2Dec/PHP/src/Controller/AbstractController.php
@@ -2,8 +2,9 @@
 
 namespace App\Controller;
 
+// $value precisa ser uma string para iniciar valores com 0, como 01 por exemplo.
 abstract class AbstractController {
-    public function render(string $viewName): void {
+    public function render(string $viewName, string $result = null): void {
         require_once "../src/Views/_partials/header.php";
         require_once "../src/Views/{$viewName}.php";
         require_once "../src/Views/_partials/footer.php";
