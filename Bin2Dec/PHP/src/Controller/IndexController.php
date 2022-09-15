c9295789d12c1cbca21f826ba47baa26efc427a7
Alexandre Almeida <alee.wxa@gmail.com>
2022-09-15 17:36:31 -0300

    feat: Add conversão do valor recebido

--- Bin2Dec/PHP/src/Controller/IndexController.php
+++ Bin2Dec/PHP/src/Controller/IndexController.php
@@ -4,6 +4,15 @@ namespace App\Controller;
 
 class IndexController extends AbstractController {
     public function Index() {
+        if ($_POST) {
+            $result = 0;
+            for ($i = 0; $i < strlen($_POST["binary"]); $i++) {
+                $result += strrev($_POST["binary"])[$i] != 0 ? 2 ** $i : 0;
+            }
+
+            parent::render("index", (string) $result);
+        }
+
         parent::render("index");
     }
 }
